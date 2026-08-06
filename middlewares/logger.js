70874bb12d39cb0b8bb6e0e08ea03866ee92c0a3
Undersolved <singh.bhupendra.h@gmail.com>
2026-08-06 09:59:24 +0530

    logger.js updated

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,7 +1,21 @@
 const fs = require("node:fs");
 
 exports.loggerMiddleware = function (req, res, next) {
-	const log = `\n [${Date.now()}] ${req.method} ${req.path}`;
-	fs.appendFileSync("logs.txt", log, "utf-8");
+	const now = new Date();
+
+	const day = String(now.getDate()).padStart(2, "0");
+	const month = String(now.getMonth() + 1).padStart(2, "0");
+	const year = String(now.getFullYear()).slice(-2);
+
+	const hours = String(now.getHours()).padStart(2, "0");
+	const minutes = String(now.getMinutes()).padStart(2, "0");
+	const seconds = String(now.getSeconds()).padStart(2, "0");
+
+	const timestamp = `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
+
+	const log = `\n[${timestamp}] ${req.method} ${req.path}`;
+
+	fs.appendFileSync("logs.txt", log, "utf8");
+
 	next();
 };
